dc60ad78c4576d434eb8ed089d960085735d014d
YoussefElhossary <YoussefElhossary@drop-eg.com>
2024-03-26 22:28:33 +0200

    AilaOnDrop

--- Aila/flutter_service_worker.js
+++ Aila/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4718194b4e4912dd01aeeb545fd8bc2a",
-"index.html": "955eed0cfcab0771a2bd4bc9d48968db",
-"/": "955eed0cfcab0771a2bd4bc9d48968db",
+"index.html": "42fbd0ef807911084f8e549219b855c5",
+"/": "42fbd0ef807911084f8e549219b855c5",
 "netlify.toml": "7e95ea2ac7e4feb0ce10455cd94464ae",
-"main.dart.js": "82aa5ed7a2231b64b20689953d0345b4",
+"main.dart.js": "70025a57220d1831ad9ca6e399c2071b",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "79f39a3a8ecac4adeb1f6cc2ca4ad1a6",
 "icons/icon-192.png": "d6e83fcfa629494bb0ce599c090987ab",
