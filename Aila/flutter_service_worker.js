dedf1e3872bd3995c8fb18db0381ccc935e2e6a5
YoussefElhossary <YoussefElhossary@drop-eg.com>
2024-05-12 11:22:04 +0300

    Minor Patch

--- Aila/flutter_service_worker.js
+++ Aila/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4718194b4e4912dd01aeeb545fd8bc2a",
-"index.html": "9e7789cbbd7dab69565afdef61f0450a",
-"/": "9e7789cbbd7dab69565afdef61f0450a",
+"index.html": "e9100078d9b90fdb4b9bc0dc321e7c43",
+"/": "e9100078d9b90fdb4b9bc0dc321e7c43",
 "netlify.toml": "7e95ea2ac7e4feb0ce10455cd94464ae",
-"main.dart.js": "c791d90b0cedab511204b8070cdf40bd",
+"main.dart.js": "a8c995af6b6f97d595a6bd9d2bf3b719",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "79f39a3a8ecac4adeb1f6cc2ca4ad1a6",
 "icons/icon-192.png": "d6e83fcfa629494bb0ce599c090987ab",
